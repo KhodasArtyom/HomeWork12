c2ddcc52b1ff1a0a7a32554f8b4b27b2139d1802
ArtyomKhodas <a.Khodas1601@gmail.com>
2023-02-13 19:16:07 +0300

    add one signature of method task1()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Шифр цезаря");
+        task1();
+
+    }
+    private static void task1() {
+        System.out.println("Решене первой задачи ");
     }
 }
---
+++ src/by/teachmeskills/KhodasArtyom/homework12/Cipher.java
@@ -0,0 +1,4 @@
+package by.teachmeskills.KhodasArtyom.homework12;
+
+public class Cipher {
+}
